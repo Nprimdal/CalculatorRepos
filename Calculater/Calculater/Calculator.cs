3decfa91e3d523a17137e1d532695ea0c2679732
Nanna Primdal <201811526@post.au.dk>
2021-02-04 13:37:02 +0100

    hej

--- Calculater/Calculater/Calculator.cs
+++ Calculater/Calculater/Calculator.cs
@@ -16,14 +16,16 @@ namespace Calculater
             return a - b;
         }
 
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
         public double Power(double x, double exp)
         {
             return Math.Pow(x, exp);
         }
 
-        public double Multiply(double a, double b)
-        {
-            return a * b;
-        }
+        
     }
 }
