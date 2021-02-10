b132fb98dd2859cc0079a449e3ef46c78e90e75a
Nanna Primdal <201811526@post.au.dk>
2021-02-10 11:11:07 +0100

    TestCases tilføjet for alle metoder, så der er 3 tests til alle metoder

--- Calculater/NUnitTestProject1/CalculatorTest.cs
+++ Calculater/NUnitTestProject1/CalculatorTest.cs
@@ -14,24 +14,34 @@ namespace NUnitTestProject1
 
         [TestCase(3.5,10,13.5)]
         [TestCase(1.7, 10, 11.7)]
-        public void Test_Add2point3And6point7_expected9(double a, double b, double expected)
+        [TestCase(5,0,5)]
+        public void Test_Add_TestCase_aAddB_EqualsExpected(double a, double b, double expected)
         {
           Assert.That(_uut.Add(a,b), Is.EqualTo(expected));
         }
-        [Test]
-        public void Test_Subtract8point3And6point2_expected9()
+
+        [TestCase(8.3, 6.2, 2.1)]
+        [TestCase(6, 3, 3)]
+        [TestCase(5, 10, -5)]
+        public void Test_Subtract_TestCase_aSubtractB_EqualsExpected(double a, double b, double expected)
         {
-            Assert.That(_uut.Subtract(8.3, 6.2), Is.EqualTo(2.1).Within(0.00001));
+            Assert.That(_uut.Subtract(a, b), Is.EqualTo(expected).Within(0.00001));
         }
-        [Test]
-        public void Test_Multiply8And2point5_expected20()
+
+        [TestCase(8, 2.5, 20)]
+        [TestCase(4, -10, -40)]
+        [TestCase(5, 0, 0)]
+        public void Test_Multiply_TestCase_aMultiplyB_EqualsExpected(double a, double b, double expected)
         {
-            Assert.That(_uut.Multiply(8, 2.5), Is.EqualTo(20).Within(0.00001));
+            Assert.That(_uut.Multiply(a, b), Is.EqualTo(expected));
         }
-        [Test]
-        public void Test_Power2And5_expected20()
+
+        [TestCase(2, 5, 32)]
+        [TestCase(4, 3, 64)]
+        [TestCase(5, 2, 25)]
+        public void Test_Power_TestCase_aPowerB_EqualsExpected(double a, double b, double expected)
         {
-            Assert.That(_uut.Power(2, 5), Is.EqualTo(32).Within(0.00001));
+            Assert.That(_uut.Power(a, b), Is.EqualTo(expected));
         }
 
 
@@ -39,7 +49,7 @@ namespace NUnitTestProject1
         [TestCase(6, 3, 2)]
         [TestCase(8, 4, 2)]
         [TestCase(4,0,0)]
-        public void Test_Divide_TestCase(double a, double b, double expected)
+        public void Test_Divide_TestCase_aDEvideB_EqualsExpected(double a, double b, double expected)
         {
             Assert.That(_uut.Divide(a,b), Is.EqualTo(expected));
         }
@@ -53,7 +63,7 @@ namespace NUnitTestProject1
             Assert.That(_uut.Accumulator, Is.EqualTo(15));
         }
 
-
+        [Test]
         public void Test_Accumulator_Is_EqualTo_Add()
         {
             _uut.Multiply(3, 5);
@@ -62,7 +72,7 @@ namespace NUnitTestProject1
 
             Assert.That(_uut.Accumulator, Is.EqualTo(5));
         }
-
+        [Test]
         public void Test_Accumulator_Is_EqualTo_divide()
         {
             _uut.Multiply(3, 5);
